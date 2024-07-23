4e79c18eb7ec0f9406c18cf5073c5c8062951821
fernanda binc <fernandacbincoletto@gmail.com>
2024-07-23 18:37:39 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,66 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
+import java.awt.*;
 import java.time.LocalDate;
+
 
 public class Main {
 
     public static void main(String[] args) {
         Curso curso1 = new Curso();
         curso1.setTitulo("curso java");
-        curso1.setDescricao("Descrição curso java");
+        curso1.setDescricao("Descricao curso java");
         curso1.setCargaHoraria(8);
 
 
         Curso curso2 = new Curso();
         curso2.setTitulo("curso JS");
-        curso2.setDescricao("Descrição curso JS");
+        curso2.setDescricao("Descricao curso JS");
         curso2.setCargaHoraria(4);
+
 
         Mentoria mentoria = new Mentoria();  curso2.setTitulo("curso JS");
         mentoria.setTitutlo("Mentoria de java");
         mentoria.setDescricao("Descricao mentoria java");
         mentoria.setData(LocalDate.now());
 
-
-
-
-
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descricao do Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscrverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos Camila:" + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        devCamila.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos inscritos Camila:" + devCamila.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Camila:" + devCamila.getConteudosConcluidos());
+        System.out.println("XP:" + devCamila.caucularTotalXP());
+
+        System.out.println("-----------");
+        Dev devFernanda = new Dev();
+        devFernanda.setNome("Fernanda");
+        devFernanda.inscrverBootcamp(bootcamp );
+        System.out.println("Conteudos inscritos Fernanda:" + devFernanda.getConteudosInscritos());
+        devFernanda.progredir();
+        devCamila.progredir();
+        devCamila.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos inscritos Fernanda:" + devFernanda.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Fernanda:" + devFernanda.getConteudosConcluidos());
+        System.out.println("XP:" + devFernanda.caucularTotalXP());
+
+
+        }
 
 
     }
-}
--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,28 +2,18 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-  private   String titutlo;
-  private  String descricao;
+
+public class Mentoria extends Conteudo {
+
   private LocalDate data;
 
+    @Override
+    public double caucularXP() {
+        return XP_PADRAO + 20d;
+    }
+
     public Mentoria() {
-    }
 
-    public String getTitutlo() {
-        return titutlo;
-    }
-
-    public void setTitutlo(String titutlo) {
-        this.titutlo = titutlo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
 
     public LocalDate getData() {
@@ -37,9 +27,13 @@ public class Mentoria {
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titutlo='" + titutlo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titutlo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", data=" + data +
                 '}';
+    }
+
+
+    public void setTitutlo(String mentoriaDeJava) {
     }
 }
